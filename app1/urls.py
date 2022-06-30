c8079d99711f24766433cbb3ffddd97ece93f862
Dilshad <dil_shad@Asus.domain.name>
2022-06-30 16:18:17 +0530

    sec commit

--- app1/urls.py
+++ app1/urls.py
@@ -3,6 +3,13 @@ from . import views
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-        path('',views.dashboard, name='dashboard' )
+        path('',views.dashboard, name='dashboard' ),
+        path('team_leader_clints',views.team_leader_clints,name='team_leader_clints'),
+        path('previous_leads',views.previous_leads,name='previous_leads'),
+        path('sales_executive',views.sales_executive,name='sales_executive'),
+        path('executive_dashboards',views.executive_dashboards,name='executive_dashboards'),
+        path('exe_previous_leads',views.exe_previous_leads,name='exe_previous_leads'),
+        path('current_leads',views.current_leads,name='current_leads'),
+        path('newly_assigned_leads',views.newly_assigned_leads,name='newly_assigned_leads'),
 
 ]
--- app1/views.py
+++ app1/views.py
@@ -3,3 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request,'dashboard_head.html')
+
+
+def team_leader_clints(request):
+    return render(request,'clints.html')
+
+
+
+def previous_leads(request):
+    return render(request,'previous_leads.html')
+
+def sales_executive(request):
+    return render(request,'counsilers_tbl.html')
+
+def executive_dashboards(request):
+    return render(request,'exe_dash.html')
+
+
+def exe_previous_leads(request):
+    return render(request,'exe_previuos_leads.html')
+
+def current_leads(request):
+    return render(request,'exe_current_leads.html')
+
+
+def newly_assigned_leads(request):
+    return render(request,'exe_newly_assigned_leads.html')
